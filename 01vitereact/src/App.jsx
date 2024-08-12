db1c3ec883f122b41d9823eab870486a39c3b25c
Habi-Pyatha <unionhab@gmail.com>
2024-08-12 11:22:29 +0545

    react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import Chai from "./chai" //"./chai" is file name chai.jsx // Chai is function inside chai.jsx
+function App() {
+  
+
+  return (
+    // <h1>This is the vite lite which is very fast| Developer Habi Pyatha</h1>
+    <Chai />
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,13 @@
+function Chai(){
+    return(
+        <>
+        {/* //using this (fragment) you can technically transfer multiple element */}
+        <h3>Chai is ready</h3>
+        <h1>This is the seond element</h1>
+        <h4>only one element can be transferd once so we are transfering only one element using fragment inside that element there is more than one element</h4>
+
+        </>
+    )
+} 
+
+export default Chai
