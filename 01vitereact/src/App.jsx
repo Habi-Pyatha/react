925a231875726a9361e2f5ff220a1726c1ef3500
Habi-Pyatha <unionhab@gmail.com>
2024-08-12 14:56:18 +0545

    hooks

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,14 @@
 import Chai from "./chai" //"./chai" is file name chai.jsx // Chai is function inside chai.jsx
 function App() {
-  
+  const username="Happy Hacking"  
 
   return (
+    <>
     // <h1>This is the vite lite which is very fast| Developer Habi Pyatha</h1>
     <Chai />
+    <p>user name is {username}</p> 
+    {/* Evalutaion expression:{username} */}
+    </>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,12 +1,46 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
 
+function MyApp(){
+    return(
+        <>
+        <div>
+            <h1>Custom App|||Habi Pyatha</h1>
+        </div>
+        </>
+    )
+}
+// const reactElement={
+//     type:'a',
+//     props:{
+//         href:'https://google.com',
+//         target:'_blank',
+
+//     },
+//     children:'click me to visit google'
+// }
+const anotherElement=(
+    <a href="https://google.com" target="_blank">Visit Google</a>
+)
+// const anotherUser="this is another user"
+const reactElement= React.createElement(
+    'a',
+    {
+        href:'https://google.com' ,target:'_blank'
+    },
+    'click me to visit google'
+    
+)
 createRoot(document.getElementById('root')).render(
 //   <StrictMode>
 //   <App />
 // </StrictMode>
-
-<App />
+//<MyApp />
+//{/* <App /> */}
+// reactElement
+// anotherElement
+reactElement
   
 )
